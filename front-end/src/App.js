a0c46f09e5f033bc4b823f18fa18be55b26c82c7
uyenMUC <minhuyen7d@gmail.com>
2024-11-09 18:34:04 +0700

    create-map

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapComponent from './MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Max flow problem</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
